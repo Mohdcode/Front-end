515899cf2a2fc97e391b49c68bb72cafb9f468b6
Mohd Kamaal <mohdkamaal2019@gmail.com>
2023-11-27 13:21:02 +0530

    added the projects

---
+++ newsApp/script.js
@@ -0,0 +1,69 @@
+const API_key = "69ebe201ae5248a5a844b6d3d7ef36f0";
+const url = "https://newsapi.org/v2/everything?q=";
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Your existing code here
+
+    // For example, the window load event can be moved here as well
+    window.addEventListener('load', () => fetchNews("India"));
+});
+
+
+async function fetchNews(query) {
+    const res = await fetch(`${url}${query}&apiKey=${API_key}`);
+    const data = await res.json();
+    bindData(data.articles);
+}
+
+function bindData(articles) {
+    const cardsContainer = document.getElementById('cards-container');
+    const newsCardTemplate = document.getElementById('template-news-card');
+    cardsContainer.innerHTML = '';
+    articles.forEach((article) => {
+        if (!article.urlToImage) return;
+        const cardClone = newsCardTemplate.content.cloneNode(true);
+        fillDataInCard(cardClone, article);
+        cardsContainer.appendChild(cardClone);
+    });
+}
+
+function fillDataInCard(cardClone, article) {
+    const newsImg = cardClone.querySelector('#news-img');
+    const newsTitle = cardClone.querySelector('#news-title');
+    const newsSource = cardClone.querySelector('#news-source');
+    const newsDesc = cardClone.querySelector('#news-desc');
+
+    newsImg.src = article.urlToImage;
+    newsTitle.innerHTML = article.title;
+    newsDesc.innerHTML = article.description;
+    const date = new Date(article.publishedAt).toLocaleString("eng-US", {
+        timeZone: "Asia/Jakarta"
+    });
+    newsSource.innerHTML = `${article.source.name} ' ${date}`;
+    cardClone.firstElementChild.addEventListener("click",()=>{
+        window.open(article.url,"_blank");
+    })
+}
+let curlSelectedNav=null;
+
+function onNavitemClick(id) {
+    fetchNews(id);
+    const navitem = document.getElementById(id);
+   curlSelectedNav?.classList.remove('active');
+   curlSelectedNav=navitem;
+   curlSelectedNav.classList.add('active');
+}
+
+const searchButton=document.getElementById('search-button');
+const searchText=document.getElementById('search-text');
+
+searchButton.addEventListener('click',()=>{
+    const query=searchText.value;
+    if(!query)return;
+    fetchNews(query);
+    curlSelectedNav?.classList.remove("active");
+    curlSelectedNav=null;
+})
+function reload(){
+    window.location.reload();
+}
---
+++ weather-app/script.js
@@ -0,0 +1,59 @@
+const apiKey="d63976b6dda187968cbe784c81dc8ab4";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?q";
+const searchBox=document.querySelector(".search input");
+const searchBtn=document.querySelector(".search button");
+const weatherIcon=document.querySelector(".weather-icon")
+
+
+
+async function checkweather (city){
+    const response=await fetch(apiUrl+`=${city}&appid=${apiKey}&units=metric`);
+    if (response.status == 404){
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display = "none";
+
+    }else{
+        var data=await response.json();
+        console.log(data);
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+"°c";
+        document.querySelector(".humidity").innerHTML = data.main.humidity+ "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed+ "km/hr";
+    
+        if(data.weather[0].main=="Clouds"){
+            weatherIcon.src="./weather-app-img/images/clouds.png"
+    
+        }
+        else if(data.weather[0].main=="Clear"){
+            weatherIcon.src="./weather-app-img/images/clear.png"
+    
+        }
+        else if(data.weather[0].main=="Rain"){
+            weatherIcon.src="./weather-app-img/images/rain.png"
+    
+        }
+        else if(data.weather[0].main=="Drizzle"){
+            weatherIcon.src="./weather-app-img/images/drizzle.png"
+    
+        }
+        else if(data.weather[0].main=="Mist"){
+            weatherIcon.src="./weather-app-img/images/mist.png"
+    
+        }
+        document.querySelector(".error").style.display="none";
+
+        document.querySelector(".weather").style.display = "block";
+    }
+   
+
+}
+searchBtn.addEventListener("click",()=>{
+    checkweather(searchBox.value)
+})
+function keyPress(event){
+    if (event.key === 'Enter' || event.keyCode === 13) {
+        event.preventDefault();
+        checkweather(searchBox.value);
+
+      }
+}
